3beea17cdd96fb0de91628e5b8e75597feb9957e
Fed-or <fedor.oryshchuk@gmail.com>
2019-03-01 13:49:13 +0200

    made mastr

---
+++ src/com/fediroryshchuk/homework3/branches/OneBranch.java
@@ -0,0 +1,8 @@
+package com.fediroryshchuk.homework3.branches;
+
+public class OneBranch {
+
+    public static void main(String[] args) {
+        System.out.println("Create master, first commit");
+    }
+}
