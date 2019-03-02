9676a812b4718dcc31797b2706595a2549de57be
Fed-or <fedor.oryshchuk@gmail.com>
2019-03-02 09:38:00 +0200

    post review

--- src/com/fediroryshchuk/homework3/carconstructor/CarWheel.java
+++ src/com/fediroryshchuk/homework3/carconstructor/CarWheel.java
@@ -3,29 +3,28 @@ package com.fediroryshchuk.homework3.carconstructor;
 public class CarWheel {
 
     private double tyreIntegrity;
+    private static final double GOOD_TYRE = 1;
+    private static final double BAD_TYRE = 0;
+
 
     public CarWheel() {
-        tyreIntegrity = 1;
+        tyreIntegrity = GOOD_TYRE;
     }
 
     public CarWheel(double stateWheels) {
-        if (stateWheels > 0 && stateWheels <= 1) {
+        if (stateWheels > BAD_TYRE && stateWheels <= GOOD_TYRE) {
             this.tyreIntegrity = stateWheels;
         }
     }
 
     public void changeTyre() {
-        if (tyreIntegrity > 0 && tyreIntegrity < 0.7) {
-            tyreIntegrity = 1;
+        if (tyreIntegrity > 0 && tyreIntegrity < 0.5) {
+            tyreIntegrity = GOOD_TYRE;
         }
     }
 
-    public void toWearTyre(int year) {
-        int speed;
-
-        for (speed = 0; speed < new Car(year).getMaxSpeed(); speed += 10) {
-            tyreIntegrity -= 0.005;
-        }
+    public void toWipeTyre(double wipe) {
+        tyreIntegrity = ((tyreIntegrity - wipe/100) > 0) ? tyreIntegrity : 0;
     }
 
     public double stateTyre() {
